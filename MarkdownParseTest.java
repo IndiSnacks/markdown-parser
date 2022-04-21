517e733820aa55fe23714e4fc93710f48e7df554
IndiSnacks <65885329+IndiSnacks@users.noreply.github.com>
2022-04-21 14:49:16 -0700

    All tests work

---
+++ MarkdownParseTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*; //imports the JUnit assert api?
+import org.junit.*; //imports the JUnit lib
+
+public class MarkdownParseTest{
+    @Test 
+    public void addition() { 
+        assertEquals(2, 1 + 1); //test if 2 equals the value of 1+1
+    } 
+} 
+
