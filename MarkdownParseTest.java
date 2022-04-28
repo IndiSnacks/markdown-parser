f79d0cb0c56e82717cf93f77cc0301564d679aab
IndiSnacks <65885329+IndiSnacks@users.noreply.github.com>
2022-04-28 14:16:00 -0700

    fixed tester

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -21,8 +21,6 @@ public class MarkdownParseTest{
     public void testfile2() throws IOException{
         Path fileName = Path.of("test-file2.md");
         String content = Files.readString(fileName);
-        ArrayList<String> test = MarkdownParse.getLinks(content);
-        System.out.println("HERE" + test);
         assertEquals(List.of("https://hello.net", "some.html"), test);
     }
 } 
